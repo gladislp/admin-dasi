55fe379790fb494415fc87c00f3dc95b9ab46806
gladislp <gladisgadis93@gmail.com>
2026-03-10 11:34:04 +0700

    EditProduct.jsx

---
+++ src/pages/products/EditProduct.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+import AdminLayout from "../../components/layout/AdminLayout";
+
+export default function EditProduct() {
+  return (
+    <AdminLayout>
+    <div className="space-y-6">
+      ...
+    </div>
+    </AdminLayout>
+  );
+}
