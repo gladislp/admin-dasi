55c648bf4512e2cd47d212cc72770b58bb631d9e
gladislp <gladisgadis93@gmail.com>
2026-03-10 11:34:26 +0700

    AddProduct.jsx

---
+++ src/pages/products/AddProduct.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+import AdminLayout from "../../components/layout/AdminLayout";
+
+export default function AddProduct() {
+  return (
+    <AdminLayout>
+    <div className="space-y-6">
+      ...
+    </div>
+    </AdminLayout>
+  );
+}
